790858c954a7097d22ed4be267e0de2632c107f0
Mateusz Stopa <matistopa2001@gmail.com>
2021-06-23 17:11:11 +0200

    button deleted from artilce.dogs

--- js/main.js
+++ js/main.js
@@ -4,9 +4,19 @@ const menuContent = document.querySelector('aside');
 const showMenu = () => {
     menuIcon.classList.toggle('active');
     menuContent.classList.toggle('active');
-    console.log(menuIcon.classList)
-    console.log(menuContent.classList)
 }
 
+/*
+const showText = () => {
+    paragraphEddie.classList.toggle('show');
+    if(paragraphEddie.classList.contains('show')){ 
+        paragraphEddie.textContent = currentTextEddie + showTextEddie;
+        btnEddie.textContent = "Zobacz mniej";
+    }else{
+        paragraphEddie.textContent = currentTextEddie;
+        btnEddie.textContent = "Zobacz więcej";
+    }
+}
+*/
 menuIcon.addEventListener('click', showMenu);
 menuContent.addEventListener('click', showMenu);
